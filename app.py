cde9fcf5891c351213ef1137fd8fb196f14a742b
Chirag Sharma <brandcrs04@gmail.com>
2025-05-18 23:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
 import google.generativeai as genai
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 import streamlit as st
 import pdfplumber
